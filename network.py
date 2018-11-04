8af4440adfc66d348d76b0bf281871f715193ed1
owenpaulmeyer <owenpaulmeyer@gmail.com>
2018-11-04 15:59:27 -0800

    feedforward,sigmoid

--- network.py
+++ network.py
@@ -11,3 +11,21 @@ class Network(object):
         # Wjk ( self.weights[n][j][k] ) is the weight for the connection between the kth neuron in the nth layer and the jth neuron in the (n+1)th layer
         self.weights = [np.random.randn(y,x)
                         for x, y in zip(sizes[:-1], sizes[1:])]
+
+    # given:
+    # a′ = σ( wa + b )
+    # where w := weights of some layer n,
+    #       a := vector of activation of layer n
+    #       b := b is the vector of biases `of layer n, self.biases[n+1]` ?verify:``
+    #       σ := sigmoid function applied to each element of the vector ( wa + b )
+
+
+    def sigmoid(self, z):
+        return 1.0 / (1.) + np.exp(-z)
+
+
+    def feedforward(self, a):
+        """Return the output of the network if "a" is input."""
+        for b, w in zip(self.biases, self.weights):
+            a = self.sigmoid(np.dot(w, a) + b)
+        return a
